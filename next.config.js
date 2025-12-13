559366a4db6d78dfa81c798e7276181f62b6e93c
yuya-piova <piova.sr.yt1211@gmail.com>
2025-12-13 19:32:26 +0900

    Remove pwa for build test

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig = {
-  // PWAの設定を適用
-  ...withPWA({
-    reactStrictMode: true,
-
-    // ★重要: Turbopackのエラーを回避するため、webpack設定を空の関数として渡す
-    // next-pwaの内部で使われるwebpack設定を上書きし、Turbopackのチェックを回避する
-    webpack: (config, options) => {
-      return config;
-    },
-  }),
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
